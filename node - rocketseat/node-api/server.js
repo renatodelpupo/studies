b4a910b739d6f41aa7e850820a788d77a9ae19b0
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-07-23 15:04:49 -0300

    Resposta básica res.send - RocketNode

--- node - rocketseat/node-api/server.js
+++ node - rocketseat/node-api/server.js
@@ -2,5 +2,11 @@ const express = require('express')
 
 const app = express()
 
-app.listen(3001)
+// app.get = toda vez que o usuário acessar a rota
+// o parâmetro req simboliza a requisição ao servidor
+// o res é a resposta para a requisição
+app.get('/', (req, res) => {
+    res.send('Hello Rocket')
+})
 
+app.listen(3001)
