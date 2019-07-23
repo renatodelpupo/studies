a659e48f0cf646a4dace9bdce1b576acb3595afd
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-07-23 16:21:16 -0300

    Organizando arquivo de rotas p2 - RocketNode

---
+++ node - rocketseat/node-api/src/controllers/ProductController.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Product = mongoose.model('Product')
+
+module.exports = {
+  async index(req, res) {
+    const products = await Product.find()
+    // o await só vai deixar ir para a próxima linha quando ele concluir
+
+    return res.json(products)
+  }
+}
--- node - rocketseat/node-api/src/routes.js
+++ node - rocketseat/node-api/src/routes.js
@@ -1,18 +1,11 @@
 const express = require('express')
 const routes = express.Router()
 
+const ProductController = require('./controllers/ProductController')
+
 // app.get = toda vez que o usuário acessar a rota
 // o parâmetro req simboliza a requisição ao servidor
 // o res é a resposta para a requisição
-routes.get('/', (req, res) => {
-
-  // Product.create({
-  //     title: 'React Native',
-  //     description: 'Build native apps with React',
-  //     url: 'http://github.com/facebook/react-native'
-  // })
-
-  return res.send('Hello Rocket')
-})
+routes.get('/products', ProductController.index)
 
 module.exports = routes
