14b637b5d91ba0f2aea24774454e99fc64725655
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-07-23 17:57:13 -0300

    Criação de registro - RocketNode

--- node - rocketseat/node-api/src/controllers/ProductController.js
+++ node - rocketseat/node-api/src/controllers/ProductController.js
@@ -8,5 +8,11 @@ module.exports = {
     // o await só vai deixar ir para a próxima linha quando ele concluir
 
     return res.json(products)
+  },
+
+  async store(req, res) {
+    const product = await Product.create(req.body)
+
+    return res.json(product)
   }
 }
--- node - rocketseat/node-api/src/routes.js
+++ node - rocketseat/node-api/src/routes.js
@@ -7,5 +7,6 @@ const ProductController = require('./controllers/ProductController')
 // o parâmetro req simboliza a requisição ao servidor
 // o res é a resposta para a requisição
 routes.get('/products', ProductController.index)
+routes.post('/products', ProductController.store)
 
 module.exports = routes
