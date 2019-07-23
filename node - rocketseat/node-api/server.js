aa8a52054d08366bdae48db984e735b9aec84bdf
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-07-23 15:45:40 -0300

    npm install mongoose - RocketNode

--- node - rocketseat/node-api/server.js
+++ node - rocketseat/node-api/server.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
+// Iniciando o App
 const app = express()
+
+// Iniciando o banco de dados
+mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 
 // app.get = toda vez que o usuário acessar a rota
 // o parâmetro req simboliza a requisição ao servidor
