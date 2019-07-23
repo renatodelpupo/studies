e005e76dcfc5f1a195f9c2a5148c5fa77e73e960
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-07-23 15:58:37 -0300

    Primeiro registro no banco - RocketNode

--- node - rocketseat/node-api/server.js
+++ node - rocketseat/node-api/server.js
@@ -11,11 +11,20 @@ mongoose.connect('mongodb://localhost:27017/nodeapi', { useNewUrlParser: true })
 // Usando esse pacote, ele requere recursivamente toda a pasta
 requireDir('./src/models')
 
+const Product = mongoose.model('Product')
+
 // app.get = toda vez que o usuário acessar a rota
 // o parâmetro req simboliza a requisição ao servidor
 // o res é a resposta para a requisição
 app.get('/', (req, res) => {
-    res.send('Hello Rocket')
+
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    })
+
+    return res.send('Hello Rocket')
 })
 
 app.listen(3001)
