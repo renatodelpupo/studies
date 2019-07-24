33917450d78870d9b2936bd4fab54e2fa1516100
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-07-24 13:37:13 -0300

    Concluindo CRUD - RocketNode

--- node - rocketseat/node-api/src/routes.js
+++ node - rocketseat/node-api/src/routes.js
@@ -7,6 +7,9 @@ const ProductController = require('./controllers/ProductController')
 // o parâmetro req simboliza a requisição ao servidor
 // o res é a resposta para a requisição
 routes.get('/products', ProductController.index)
+routes.get('/products/:id', ProductController.show)
 routes.post('/products', ProductController.store)
+routes.put('/products/:id', ProductController.update)
+routes.delete('/products/:id', ProductController.destroy)
 
 module.exports = routes
