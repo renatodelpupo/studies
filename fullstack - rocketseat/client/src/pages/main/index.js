6c43d111d4b724fed9b27fade711720388f0a581
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-07-28 17:07:48 -0300

    setState na mão - Fullstack

--- fullstack - rocketseat/client/src/pages/main/index.js
+++ fullstack - rocketseat/client/src/pages/main/index.js
@@ -11,12 +11,31 @@ export default class Main extends Component {
     this.loadProducts()
   }
 
-  loadProducts = async (page = 1) => {
-    const response = await api.get(`/products?page=${page}`)
-
+  loadProducts = async () => {
+    const response = await api.get(`/products`)
     const { docs } = response.data
-
     this.setState({ products: docs })
+  }
+
+  // Se não usar a arrow function perde-se o escopo do this
+  enviarFormulario = evento => {
+    evento.preventDefault()
+
+    console.log('Enviando formulário')
+    console.log(this.state.products[0])
+
+    this.setState({
+
+      products: [
+        {
+          "_id": "1234",
+          "nome": "Caralho",
+          "email": "mermao",
+          "createdAt": "2019-07-28T18:30:59.556Z",
+          "__v": 0
+        }
+      ]
+    })
   }
 
   render() {
@@ -25,6 +44,13 @@ export default class Main extends Component {
 
     return (
       <div>
+        <form onSubmit={this.enviarFormulario}>
+          <label>Nome</label>
+          <input id="nome" type="text" name="nome" />
+          <label>E-mail</label>
+          <input id="email" type="email" name="email" />
+          <button type="submit">Gravar</button>
+        </form>
         {products.map(product => (
           <article key={product._id}>
             <strong>{product.nome}</strong>
