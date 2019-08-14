2a115d1fbced09814fd8dc987da964a7b1e44405
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-08-14 11:35:08 -0300

    Separando Bundle (nosso) de Vendor (bibliotecas) - WA
    Aula 6 - Atividade 3

--- webpack - alura/client/webpack.config.js
+++ webpack - alura/client/webpack.config.js
@@ -11,6 +11,10 @@ plugins.push(new webpack.ProvidePlugin({
   '$': 'jquery/dist/jquery.js',
   'jQuery': 'jquery/dist/jquery.js'
 }))
+plugins.push(new webpack.optimize.CommonsChunkPlugin({
+  name: 'vendor',
+  filename: 'vendor.bundle.js'
+}))
 
 if (process.env.NODE_ENV == 'production') {
 
@@ -31,7 +35,10 @@ if (process.env.NODE_ENV == 'production') {
 }
 
 module.exports = {
-  entry: './app-src/app.js',
+  entry: {
+    app: './app-src/app.js',
+    vendor: ['jquery', 'bootstrap', 'reflect-metadata']
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
