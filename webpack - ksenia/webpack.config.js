1acf71a9f6a2362689469b665e141e146481af62
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-08-19 21:19:50 -0300

    Testes com JS - WK

--- webpack - ksenia/webpack.config.js
+++ webpack - ksenia/webpack.config.js
@@ -2,26 +2,33 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const variables = require('./variables.js');
 
+const dados = [
+  {
+    nome: 'Renato',
+    cidade: 'Vitória'
+  },
+  {
+    nome: 'Marina',
+    cidade: 'Cariacica'
+  }
+]
+
+let plugins = []
+
+dados.forEach(item => {
+  plugins.push(new HtmlWebpackPlugin({
+    template: 'index.html',
+    filename: `${item.nome}.html`,
+    title: item.nome,
+    cidade: item.cidade,
+  }))
+})
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      filename: 'renato.html',
-      title: 'Renato',
-      bar: 'bar',
-      favWord: variables.word
-    }),
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-      filename: 'marina.html',
-      title: 'Marina',
-      bar: 'bar',
-      favWord: variables.word
-    })
-  ]
-};
+  plugins: plugins
+}
