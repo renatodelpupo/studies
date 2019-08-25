ebc16a7c6e4147bc7d0c3b6e7e281061ae537fbf
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-08-25 11:39:57 -0300

    Aulas 1 e 2 - Vue Alura

---
+++ vue - alura/alurapic/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.src" :alt="foto.alt" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Starting Alurapic",
+      fotos: [
+        {
+          src:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          alt: "Happy dog"
+        },
+        {
+          src:
+            "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTwV4kVzT5McBdGSgqlVeRzubrNH_mOrrkKseDOGFURq20HmsrelEfMU7It",
+          alt: "Happy dog"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+</style>
