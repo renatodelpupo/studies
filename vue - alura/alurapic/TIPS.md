a58a3457c1da2f8a40da03d3ce59800d9a767683
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-09-08 14:24:58 -0300

    Formatação e TIPS.md

---
+++ vue - alura/alurapic/TIPS.md
@@ -0,0 +1,14 @@
+
+# View to data
+v-on or @
+Example: v-on:dblclick or @dblclick
+
+# Data to view
+v-bind or :
+Example: v-bind:titulo or :titulo
+
+# ##
+
+# RegEx
+trim() retira espaços em branco, 'i' deixa case insensitive
+let expressaoRegular = new RegExp(this.termoBusca.trim(), "i");
--- vue - alura/alurapic/src/App.vue
+++ vue - alura/alurapic/src/App.vue
@@ -3,89 +3,75 @@
   <div class="corpo">
     <h1 class="centralizado">{{ titulo }}</h1>
 
-    <input type="search" class="filtro" v-on:input="termoBusca = $event.target.value" placeholder="digite o título da foto">
-
+    <input type="search" class="filtro" @input="termoBusca = $event.target.value" placeholder="digite o título da foto" />
     {{ termoBusca }}
 
     <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosFiltradas" v-bind:key="foto.titulo">
-
+      <li class="lista-fotos-item" v-for="foto of fotosFiltradas" :key="foto.titulo">
         <meu-painel :titulo="foto.titulo">
-            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
         </meu-painel>
-
       </li>
     </ul>
-
   </div>
 </template>
 
 <script>
-import Painel from './components/shared/painel/Painel.vue';
+import Painel from "./components/shared/painel/Painel.vue";
 
 export default {
-
   components: {
-    'meu-painel' : Painel
+    "meu-painel": Painel
   },
 
   data() {
-
     return {
-
-      titulo: 'Alurapic', 
+      titulo: "Alurapic",
       fotos: [],
-      termoBusca: ''
-    }
+      termoBusca: ""
+    };
   },
 
   computed: {
-
     fotosFiltradas() {
       if (this.termoBusca) {
-        // trim() retira espaços em branco, 'i' deixa case insensitive
-        let expressaoRegular = new RegExp(this.termoBusca.trim(), 'i')
-        return this.fotos.filter(foto => expressaoRegular.test(foto.titulo))
+        let expressaoRegular = new RegExp(this.termoBusca.trim(), "i");
+        return this.fotos.filter(foto => expressaoRegular.test(foto.titulo));
       } else {
-        return this.fotos
+        return this.fotos;
       }
     }
-
   },
 
   created() {
-
-    this.$http.get('http://localhost:3000/v1/fotos')
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
       .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+      .then(fotos => (this.fotos = fotos), err => console.log(err));
   }
-}
-
+};
 </script>
 
 <style>
-  .corpo {
-    font-family: Helvetica, sans-serif;
-    width: 96%;
-    margin: 0 auto;
-  }
+.corpo {
+  font-family: Helvetica, sans-serif;
+  width: 96%;
+  margin: 0 auto;
+}
 
-  .centralizado {
+.centralizado {
+  text-align: center;
+}
 
-    text-align: center;
-  }
+.lista-fotos {
+  list-style: none;
+}
 
-  .lista-fotos {
-    list-style: none;
-  }
+.lista-fotos .lista-fotos-item {
+  display: inline-block;
+}
 
-  .lista-fotos .lista-fotos-item {
-
-    display: inline-block;
-  }
-
-  .imagem-responsiva {
-
-    width: 100%;
-  }
+.imagem-responsiva {
+  width: 100%;
+}
 </style>
