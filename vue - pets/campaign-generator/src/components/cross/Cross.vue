00c0868b06a65b3ba818cebb14e2f8e6210ef0e7
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-09-22 21:40:08 -0300

    Campaign Generator primary experimentation

---
+++ vue - pets/campaign-generator/src/components/cross/Cross.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="product-item">
+    <div class="product-display-area">
+      <img class="product-image" :src="image" alt="">
+      <span class="product-name">{{ displayName }}</span>
+    </div>
+    <div class="product-shop-area">
+      <div class="product-offer">
+        <span class="product-price-original">R$ {{ formatMoney(priceOriginal) }}</span>
+        <span class="product-price-offer-member">R$ {{ formatMoney(priceMember) }}</span>
+      </div>
+      <cta :available="available"></cta>
+      <span class="product-price-offer-customer">R$ {{ formatMoney(priceCustomer) }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import CTA from "../cta/CTA";
+
+export default {
+  props: {
+    image: {
+      required: true,
+      type: String
+    },
+    displayName: {
+      required: true,
+      type: String
+    },
+    priceOriginal: {
+      required: true,
+      type: Number
+    },
+    priceMember: {
+      required: true,
+      type: Number
+    },
+    priceCustomer: {
+      required: true,
+      type: Number
+    },
+    available: {
+      required: true,
+      type: Boolean
+    }
+  },
+
+  components: {
+    cta: CTA
+  },
+
+  methods: {
+    formatMoney(input) {
+      return input.toFixed(2).replace(".", ",");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.product-item {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 15px;
+  width: 250px;
+}
+.product-display-area {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+.product-name {
+  margin: 0.5em 0;
+  width: 100%;
+  font-weight: bold;
+}
+.product-shop-area {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+}
+.product-offer {
+  text-align: center;
+  font-weight: bold;
+}
+.product-price-original {
+  font-size: 0.8em;
+}
+.product-price-offer-customer {
+  text-align: center;
+  font-size: 0.7em;
+}
+</style>
---
+++ vue - pets/campaign-generator/src/components/cta/CTA.vue
@@ -0,0 +1,52 @@
+<template>
+  <button :class="isAvailable">{{ label }}</button>
+</template>
+
+<script>
+export default {
+  props: {
+    available: {
+      required: true,
+      default: false,
+      type: Boolean
+    }
+  },
+
+  computed: {
+    isAvailable() {
+      if (this.available == true) {
+        return "available";
+      } else {
+        return "soldout";
+      }
+    },
+
+    label() {
+      if (this.available == true) {
+        return "Add to cart";
+      } else {
+        return "Sold out";
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+button {
+  margin: 0.5em;
+  padding: 7px 14px;
+  width: 100%;
+  border-radius: 4px;
+  border: none;
+  color: white;
+}
+.available {
+  background-color: #7fbc44;
+  cursor: pointer;
+}
+.soldout {
+  background-color: gray;
+  cursor: not-allowed;
+}
+</style>
