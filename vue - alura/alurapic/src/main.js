f21744acfd8601aa3f78f7a7577a4a0b50ec13a9
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-10-13 16:03:42 -0300

    Traduzindo VeeValidate - Aula 5 - Vue Alura 2

--- vue - alura/alurapic/src/main.js
+++ vue - alura/alurapic/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRoute from 'vue-router'
 import VueResource from 'vue-resource'
+import VeeValidateMessages from '../node_modules/vee-validate/dist/locale/pt_BR'
 
 import { routes } from './routes';
 import VueRouter from 'vue-router';
@@ -11,7 +12,14 @@ import './directives/Transform';
 
 Vue.use(VueRoute)
 Vue.use(VueResource)
-Vue.use(VeeValidate)
+Vue.use(VeeValidate, {
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: {
+      messages: VeeValidateMessages
+    }
+  }
+})
 
 Vue.http.options.root = 'http://localhost:3000'
 
