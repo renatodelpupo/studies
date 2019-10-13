7d7cacd1af918b8c1cf1a0939870e80050a08a0d
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-10-13 15:38:24 -0300

    Instalando VeeValidate - Aula 5 - Vue Alura 2

--- vue - alura/alurapic/src/main.js
+++ vue - alura/alurapic/src/main.js
@@ -5,11 +5,13 @@ import VueResource from 'vue-resource'
 
 import { routes } from './routes';
 import VueRouter from 'vue-router';
+import VeeValidate from 'vee-validate';
 
 import './directives/Transform';
 
 Vue.use(VueRoute)
 Vue.use(VueResource)
+Vue.use(VeeValidate)
 
 Vue.http.options.root = 'http://localhost:3000'
 
