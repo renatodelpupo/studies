ed8641e343e3da685aa1a1f57c85e4c395bf4483
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-12-21 19:14:50 -0300

    Prologue - JSA1

---
+++ javascript/_alura/avançado-i/client/js/index.js
@@ -0,0 +1,40 @@
+
+const 
+  myFormInputs = [
+    document.querySelector('#date'),
+    document.querySelector('#amount'),
+    document.querySelector('#price')
+  ],
+  myTable = document.querySelector('.table')
+
+document.querySelector('.form').addEventListener('submit', event => {
+
+  event.preventDefault()  
+
+  const 
+    myFormData = myFormInputs.map(item => item.value),
+    myTableRow = document.createElement('tr')
+
+  myFormData.forEach(item => {
+    const myTableData = document.createElement('td')
+    myTableData.innerText = item
+    myTableRow.appendChild(myTableData)
+  })
+
+  const myTableSubtotal = document.createElement('td')
+  myTableSubtotal.innerText = myFormData[1] * myFormData[2]
+  myTableRow.appendChild(myTableSubtotal)
+
+  myTable.appendChild(myTableRow)
+
+  eraseMyFormInputs()
+
+})
+
+const eraseMyFormInputs = () => {
+  myFormInputs[0].value = ''
+  myFormInputs[1].value = 1
+  myFormInputs[2].value = ''
+
+  myFormInputs[0].focus()
+}
