f50a0be732e9814ebf92e1a2812cd328b4554bf0
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-12-22 11:52:09 -0300

    Creating NegotiationList - Lesson 4 - JSA1

--- javascript/_alura/avançado-i/client/js/app/controllers/NegotiationController.js
+++ javascript/_alura/avançado-i/client/js/app/controllers/NegotiationController.js
@@ -6,17 +6,28 @@ class NegotiationController {
     this._inputAmount = $('#amount')
     this._inputDate = $('#date')
     this._inputPrice = $('#price')
+    this._negotiationList = new NegotiationList()
   }
 
   add(event) {
     event.preventDefault()
+    this._negotiationList.add(this._createNegotiation())
+    this._cleanForm()
+  }
 
-    let negotiation = new Negotiation(
-        this._inputAmount.value,
-        DateHelper.stringToDate(this._inputDate.value),
-        this._inputPrice.value
-      )
+  _cleanForm() {
+    this._inputAmount.value = 1
+    this._inputDate.value = ''
+    this._inputPrice.value = 0
+    this._inputDate.focus()
+  }
 
+  _createNegotiation() {
+    return new Negotiation(
+      this._inputAmount.value,
+      DateHelper.stringToDate(this._inputDate.value),
+      this._inputPrice.value
+    )
   }
 
 }
---
+++ javascript/_alura/avançado-i/client/js/app/models/NegotiationList.js
@@ -0,0 +1,15 @@
+class NegotiationList {
+
+  constructor() {
+    this._negotiations = []
+  }
+
+  add(negotiation) {
+    this._negotiations.push(negotiation)
+  }
+
+  get negotiations() {
+    return this._negotiations
+  }
+
+}
