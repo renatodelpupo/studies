4e88205b624ac195fc91e56cdb55685a4aadbf93
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2019-12-22 11:32:00 -0300

    RegEx test - Lesson 4 - JSA1

--- javascript/_alura/avançado-i/client/js/app/helpers/DateHelper.js
+++ javascript/_alura/avançado-i/client/js/app/helpers/DateHelper.js
@@ -5,11 +5,17 @@ class DateHelper {
   }
 
   static dateToString(date) {
-    return `${ date.getDate() }/${ (date.getMonth() + 1) }/${ date.getFullYear() }`
+    return `${ date.getDate() }/${ date.getMonth() + 1 }/${ date.getFullYear() }`
   }
 
-  static stringToDate(texto) {
-    return new Date(...texto.split('-').map((item,indice) => item - indice % 2))
+  static stringToDate(text) {
+
+    if (/\d{4}-\d{2}-\d{2}/.test(text)) {
+      return new Date(...text.split('-').map((item,index) => item - index % 2))
+    } else {
+      throw new Error('Date must be in the format YYYY-MM-DD')
+    }
+
   }
 
 }
