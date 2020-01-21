540b76184114482d542278d78b66de37b3082223
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-01-20 21:42:54 -0300

    Stylizing the component - RNRH

--- react-native/_rocketseat/hunt/src/pages/main.js
+++ react-native/_rocketseat/hunt/src/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import api from '../services/api'
 
-import { View, Text, FlatList, TouchableOpacity } from 'react-native'
+import { View, Text, FlatList, TouchableOpacity, StyleSheet } from 'react-native'
 
 export default class Main extends Component {
   static navigationOptions = {
@@ -24,19 +24,20 @@ export default class Main extends Component {
   }
 
   renderItem = ({ item }) => (
-    <View>
-      <Text>{ item.nome }</Text>
-      <Text>{ item.email }</Text>
-      <TouchableOpacity onPress={ () => {} }>
-        <Text>Acessar</Text>
+    <View style={ styles.productContainer }>
+      <Text style={ styles.productName }>{ item.nome }</Text>
+      <Text style={ styles.productEmail }>{ item.email }</Text>
+      <TouchableOpacity onPress={ () => {} } style={ styles.productButton }>
+        <Text style={ styles.productButtonText }>Acessar</Text>
       </TouchableOpacity>
     </View>
   )
 
   render() {
     return (
-      <View>
+      <View style={ styles.container }>
         <FlatList
+          contentContainerStyle = { styles.list }
           data = { this.state.docs }
           keyExtractor = { item => item._id }
           renderItem = { this.renderItem }
@@ -45,3 +46,53 @@ export default class Main extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fafafa',
+    flex: 1
+  },
+
+  list: {
+    padding: 20
+  },
+
+  productButton: {
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    borderRadius: 5,
+    borderColor: '#da552f',
+    borderWidth: 2,
+    height: 42,
+    justifyContent: 'center',
+    marginTop: 10
+  },
+
+  productButtonText: {
+    color: '#da552f',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+
+  productContainer: {
+    backgroundColor: '#fff',
+    borderColor: '#ddd',
+    borderRadius: 5,
+    borderWidth: 1,
+    marginBottom: 20,
+    padding: 20
+  },
+
+  productEmail: {
+    color: '#999',
+    fontSize: 16,
+    lineHeight: 24,
+    marginTop: 5
+  },
+
+  productName: {
+    color: '#333',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+})
