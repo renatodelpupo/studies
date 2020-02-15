e0419ecb236b9a7a5cd4562eb5e3ee82778c6b32
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-02-15 17:27:01 -0300

    Updating to react-router-dom - RA1

--- react/_alura/parte-1/src/index.js
+++ react/_alura/parte-1/src/index.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, IndexRoute, browserHistory } from 'react-router'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import App from './App'
 import AutorBox from './Autor'
 import Home from './Home'
 import './index.css'
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="/autor" component={AutorBox} />
-      <Route path="/livro" />
-    </Route>
+  <Router>
+    <App>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/autor" component={AutorBox} />
+        <Route path="/livro" />
+      </Switch>
+    </App>
   </Router>,
   document.getElementById('root')
 )
