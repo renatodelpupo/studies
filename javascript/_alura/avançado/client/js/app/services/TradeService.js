529095d8c1e173cc668efb897fc9af2f336f2692
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-03-01 20:08:53 -0300

    Flattening the code - JSA2

--- javascript/_alura/avançado/client/js/app/services/TradeService.js
+++ javascript/_alura/avançado/client/js/app/services/TradeService.js
@@ -5,44 +5,32 @@ class TradeService {
   }
 
   importWeeklyTrades() {
-
-      return this.http
-        .get('trades/week')
-        .then(responseText => {
-          const trades = responseText.map(object => new Trade(object.amount, new Date(object.date), object.price))
-          return trades
-        })
-        .catch(error => {
-          console.log(error)
-          throw new Error('Weekly trades import failed')
-        })
+    return this.http
+      .get('trades/week')
+      .then(responseText => responseText.map(object => new Trade(object.amount, new Date(object.date), object.price)))
+      .catch(error => {
+        console.log(error)
+        throw new Error('Weekly trades import failed')
+      })
   }
 
   importPreviousTrades() {
-
-      return this.http
-        .get('trades/previous')
-        .then(responseText => {
-          const trades = responseText.map(object => new Trade(object.amount, new Date(object.date), object.price))
-          return trades
-        })
-        .catch(error => {
-          console.log(error)
-          throw new Error('Previous trades import failed')
-        })
+    return this.http
+      .get('trades/previous')
+      .then(responseText => responseText.map(object => new Trade(object.amount, new Date(object.date), object.price)))
+      .catch(error => {
+        console.log(error)
+        throw new Error('Previous trades import failed')
+      })
   }
 
   importOldTrades() {
-
-      return this.http
-        .get('trades/old')
-        .then(responseText => {
-          const trades = responseText.map(object => new Trade(object.amount, new Date(object.date), object.price))
-          return trades
-        })
-        .catch(error => {
-          console.log(error)
-          throw new Error('Old trades import failed')
-        })
+    return this.http
+      .get('trades/old')
+      .then(responseText => responseText.map(object => new Trade(object.amount, new Date(object.date), object.price)))
+      .catch(error => {
+        console.log(error)
+        throw new Error('Old trades import failed')
+      })
   }
 }
