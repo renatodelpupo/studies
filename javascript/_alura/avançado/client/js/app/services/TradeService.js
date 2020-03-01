780ddabbf3fa0d2381a7a067ddcbd18648765689
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-03-01 20:03:10 -0300

    Simplifying TradeService with no new Promise - JSA2

--- javascript/_alura/avançado/client/js/app/services/TradeService.js
+++ javascript/_alura/avançado/client/js/app/services/TradeService.js
@@ -6,49 +6,43 @@ class TradeService {
 
   importWeeklyTrades() {
 
-    return new Promise((resolve, reject) => {
-      this.http
+      return this.http
         .get('trades/week')
         .then(responseText => {
           const trades = responseText.map(object => new Trade(object.amount, new Date(object.date), object.price))
-          resolve(trades)
+          return trades
         })
         .catch(error => {
           console.log(error)
-          reject('Weekly trades import failed')
+          throw new Error('Weekly trades import failed')
         })
-    })
   }
 
   importPreviousTrades() {
 
-    return new Promise((resolve, reject) => {
-      this.http
+      return this.http
         .get('trades/previous')
         .then(responseText => {
           const trades = responseText.map(object => new Trade(object.amount, new Date(object.date), object.price))
-          resolve(trades)
+          return trades
         })
         .catch(error => {
           console.log(error)
-          reject('Previous trades import failed')
+          throw new Error('Previous trades import failed')
         })
-    })
   }
 
   importOldTrades() {
 
-    return new Promise((resolve, reject) => {
-      this.http
+      return this.http
         .get('trades/old')
         .then(responseText => {
           const trades = responseText.map(object => new Trade(object.amount, new Date(object.date), object.price))
-          resolve(trades)
+          return trades
         })
         .catch(error => {
           console.log(error)
-          reject('Old trades import failed')
+          throw new Error('Old trades import failed')
         })
-    })
   }
 }
