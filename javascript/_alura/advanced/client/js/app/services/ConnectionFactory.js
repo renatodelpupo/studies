ec22ecf1798cb3ec932b5eb7fb286f144ad13fe9
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-03-03 21:15:22 -0300

    Wrapping the ConnectionFactory - JSA3

--- javascript/_alura/advanced/client/js/app/services/ConnectionFactory.js
+++ javascript/_alura/advanced/client/js/app/services/ConnectionFactory.js
@@ -1,36 +1,40 @@
 const dbName = 'aluraframe'
 const stores = ['trades']
 const version = 1
+let connection = null
 
-class ConnectionFactory {
+const ConnectionFactory = (function() {
+  return class ConnectionFactory {
 
-  constructor() {
-    throw new Error('ConnectionFactory instances cannot be created')
+    constructor() {
+      throw new Error('ConnectionFactory instances cannot be created')
+    }
+
+    static getConnection() {
+      return new Promise((resolve, reject) => {
+        const openRequest = window.indexedDB.open(dbName, version)
+
+        openRequest.onupgradeneeded = event => {
+          ConnectionFactory._createStores(event.target.result)
+        }
+
+        openRequest.onsuccess = event => {
+          if (!connection) connection = event.target.result
+          resolve(connection)
+        }
+
+        openRequest.onerror = event => {
+          console.log(event.target.error)
+          reject(event.target.error.name)
+        }
+      })
+    }
+
+    static _createStores(connection) {
+      stores.forEach(store => {
+        if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store)
+        connection.createObjectStore(store, { autoIncrement: true })
+      })
+    }
   }
-
-  static getConnection() {
-    return new Promise((resolve, reject) => {
-      const openRequest = window.indexedDB.open(dbName, version)
-
-      openRequest.onupgradeneeded = event => {
-        ConnectionFactory._createStores(event.target.result)
-      }
-
-      openRequest.onsuccess = event => {
-        resolve(event.target.result)
-      }
-
-      openRequest.onerror = event => {
-        console.log(event.target.error)
-        reject(event.target.error.name)
-      }
-    })
-  }
-
-  static _createStores(connection) {
-    stores.forEach(store => {
-      if (connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store)
-      connection.createObjectStore(store, { autoIncrement: true })
-    })
-  }
-}
+})()
