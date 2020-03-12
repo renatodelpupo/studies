3ab88fd98f5ab92418314aad7c415c1ae39d4eef
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-03-11 21:04:34 -0300

    Readonly properties - ATS2

--- typescript/_alura/alurabank/app/ts/models/Trade.ts
+++ typescript/_alura/alurabank/app/ts/models/Trade.ts
@@ -1,20 +1,8 @@
 export class Trade {
 
-  constructor(private _amount: number, private _date: Date, private _price: number) { }
-
-  get amount() {
-    return this._amount
-  }
-
-  get date() {
-    return this._date
-  }
-
-  get price() {
-    return this._price
-  }
+  constructor(readonly amount: number, readonly date: Date, readonly price: number) { }
 
   get subtotal() {
-    return this._amount * this._price
+    return this.amount * this.price
   }
 }
