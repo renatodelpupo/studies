c6a64cf5b957bfe0030da1a6568e0943f825a297
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-03-11 21:11:05 -0300

    Escaping optional params - ATS2

--- typescript/_alura/alurabank/app/ts/views/View.ts
+++ typescript/_alura/alurabank/app/ts/views/View.ts
@@ -1,14 +1,20 @@
 export abstract class View<T> {
 
-  private _element: JQuery
+  protected _element: JQuery
+  private _escape: boolean
 
-  constructor(selector: string) {
+  constructor(selector: string, escape?: boolean) {
     this._element = $(selector)
+    this._escape = escape
   }
 
   abstract template(model: T): string
 
   update(model: T) {
+    let template = this.template(model)
+
+    if (this._escape) template = template.replace(/<script>[\s\S]*?<\/script>/, '')
+
     this._element.html(this.template(model))
   }
 }
