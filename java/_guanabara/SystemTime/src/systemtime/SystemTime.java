bc38ee4ecc0580c5ec8dabcaf98824e5cddbe713
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-06-11 12:39:38 -0300

    Get system time

---
+++ java/_guanabara/SystemTime/src/systemtime/SystemTime.java
@@ -0,0 +1,13 @@
+package systemtime;
+
+import java.util.Date;
+
+public class SystemTime {
+
+  public static void main(String[] args) {
+    Date clock = new Date();
+    System.out.println("The time is:");
+    System.out.println(clock);
+  }
+
+}
