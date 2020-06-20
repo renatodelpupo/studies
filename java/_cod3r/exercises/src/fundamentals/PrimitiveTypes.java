aa4652d4a05702685fe3726b7053fbdc24ae0d51
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-06-20 14:58:11 -0300

    Primitive Types

---
+++ java/_cod3r/exercises/src/fundamentals/PrimitiveTypes.java
@@ -0,0 +1,26 @@
+package fundamentals;
+
+public class PrimitiveTypes {
+
+	public static void main(String[] args) {
+		// Employee register
+
+		// Integer Literals
+		byte companyYears = 21;
+		short numberOfFlights = 471;
+		int id = 22470;
+		long cpf = 443_774_670_85L; // Need to put an L at the end
+
+		// Floating-Point Literals
+		float wage = 7489.41F; // Need to put an F at the end
+		double wageYear = 99609.153;
+
+		// Character and String Literals
+		char genre = 'M'; // Single quote
+		String name = "Odin"; // Double quote
+
+		// Boolean
+		boolean active = true;
+	}
+
+}
