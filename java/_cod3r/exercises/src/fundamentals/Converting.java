133537d254c8d19b401e8982869f5e0bf3223dba
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-06-21 19:36:25 -0300

    Converting

---
+++ java/_cod3r/exercises/src/fundamentals/Converting.java
@@ -0,0 +1,21 @@
+package fundamentals;
+
+public class Converting {
+
+	public static void main(String[] args) {
+
+		double a = 1; // implicit
+		System.out.println(a);
+
+		float b = (float) 1.234567; // explicit - cast
+		System.out.println(b);
+
+		byte c = (byte) 741; // explicit - cast
+		System.out.println(c);
+
+		int d = (int) 1.99; // explicit - cast
+		System.out.println(d);
+
+	}
+
+}
