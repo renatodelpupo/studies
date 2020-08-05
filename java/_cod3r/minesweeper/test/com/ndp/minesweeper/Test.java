fec426e574123482d1270a5d65b6e76c27107ae8
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-04 22:40:58 -0300

    Set up JUnit

---
+++ java/_cod3r/minesweeper/test/com/ndp/minesweeper/Test.java
@@ -0,0 +1,17 @@
+package com.ndp.minesweeper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+  @org.junit.jupiter.api.Test
+  void isFour() {
+    int a = 2 + 2;
+    assertEquals(4, a);
+  }
+
+  @org.junit.jupiter.api.Test
+  void isFourteen() {
+    int a = 7 + 7;
+    assertEquals(14, a);
+  }
+}
