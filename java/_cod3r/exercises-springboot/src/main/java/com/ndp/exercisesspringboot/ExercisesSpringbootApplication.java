ed9eeb01ae54dc11dd9cf8d3d0cc7d73715de5e1
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-16 17:25:38 -0300

    Start Spring

---
+++ java/_cod3r/exercises-springboot/src/main/java/com/ndp/exercisesspringboot/ExercisesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ndp.exercisesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExercisesSpringbootApplication.class, args);
+	}
+
+}
---
+++ java/_cod3r/exercises-springboot/src/test/java/com/ndp/exercisesspringboot/ExercisesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndp.exercisesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
