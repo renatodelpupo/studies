54985a4d395fd39b2c2d3fc53ede3eea530de347
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-16 21:32:49 -0300

    Web Service request parameters

--- java/_cod3r/exercises-springboot/src/main/java/com/ndp/exercisesspringboot/controllers/CustomerController.java
+++ java/_cod3r/exercises-springboot/src/main/java/com/ndp/exercisesspringboot/controllers/CustomerController.java
@@ -1,7 +1,9 @@
 package com.ndp.exercisesspringboot.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ndp.exercisesspringboot.models.Customer;
@@ -13,5 +15,15 @@ public class CustomerController {
   @GetMapping("/1")
   public Customer getCustomer() {
     return new Customer("123.456.789-00", 1, "Marina");
+  }
+
+  @GetMapping("/{id}")
+  public Customer getCustomerById(@PathVariable int id) {
+    return new Customer("234.567.891-00", id, "Luciana");
+  }
+
+  @GetMapping
+  public Customer getCustomerById2(@RequestParam(name = "id", defaultValue = "0") int id) {
+    return new Customer("345.678.912-00", id, "Renato");
   }
 }
