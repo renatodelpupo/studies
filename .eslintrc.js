39f3f8bc36c2574876bdeca06ea795d39c172638
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-23 18:22:15 -0300

    refactor(config): apply new lint style

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: ['plugin:vue/essential', '@vue/standard', '@vue/typescript'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'space-before-function-paren': ['error', 'never']
+  }
+}
