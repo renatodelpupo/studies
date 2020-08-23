804bb9c2b3d1e3671052dc69779d0b82064efdc8
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-23 18:53:28 -0300

    chore: communicating events - lesson 10

--- vue/_vuemastery/intro-to-vue-3/lesson-9/components/ProductDisplay.js
+++ vue/_vuemastery/intro-to-vue-3/lesson-9/components/ProductDisplay.js
@@ -36,6 +36,15 @@ app.component('product-display', {
             v-on:click="addToCart">
             Add to Cart
           </button>
+
+          <button
+            class="button"
+            :class="{ disabledButton: !inStock }"
+            :disabled="!inStock"
+            @click="removeFromCart">
+            Remove Item
+          </button>
+
         </div>
       </div>
     </div>
@@ -54,10 +63,13 @@ app.component('product-display', {
   },
   methods: {
     addToCart() {
-      this.cart += 1
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
     },
     updateVariant(index) {
       this.selectedVariant = index
+    },
+    removeFromCart() {
+      this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
     }
   },
   computed: {
