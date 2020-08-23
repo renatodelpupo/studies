97678181ea499bcb964ff9ceb8c426add04354bb
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-08-23 18:57:13 -0300

    fix: assets path

--- vue/_vuemastery/intro-to-vue-3/lesson-9/components/ProductDisplay.js
+++ vue/_vuemastery/intro-to-vue-3/lesson-9/components/ProductDisplay.js
@@ -56,8 +56,8 @@ app.component('product-display', {
       selectedVariant: 0,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
+        { id: 2234, color: 'green', image: '../assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: '../assets/images/socks_blue.jpg', quantity: 0 }
       ]
     }
   },
