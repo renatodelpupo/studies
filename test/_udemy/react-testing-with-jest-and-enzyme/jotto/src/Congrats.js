0d75f887e0b17a0aca845feaa9ffdec85fab3802
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-12-02 22:40:44 -0300

    chore: init congrats component

---
+++ test/_udemy/react-testing-with-jest-and-enzyme/jotto/src/Congrats.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+/**
+ * Functional React component for congratulatory message
+ * @function
+ * @returns {JSX.Element} - Rendered component Rendered component (or null if `success` prop is false).
+ */
+
+export default () => {
+  return <div></div>
+}
