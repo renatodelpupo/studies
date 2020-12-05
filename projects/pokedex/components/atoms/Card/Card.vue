e39882425237414ae8a7e8057a17892652dbe998
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-12-05 17:59:11 -0300

    chore: create monster card

---
+++ projects/pokedex/components/atoms/Card/Card.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="card" :class="dynamicClass">
+    <div class="card-left">
+      <div class="image">
+        <slot name="image" />
+      </div>
+    </div>
+
+    <div class="card-right">
+      <div class="number">
+        <span v-text="number" />
+      </div>
+
+      <div class="name">
+        <slot name="name" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+
+  props: {
+    id: {
+      default: 0,
+      type: Number
+    },
+    mainType: {
+      default: '',
+      type: String
+    }
+  },
+
+  computed: {
+    dynamicClass() {
+      return [`background-color-${this.mainType}`]
+    },
+
+    number() {
+      const paddedNumber = this.id.toString().padStart(3, '0')
+      return `Nº${paddedNumber}`
+    }
+  }
+}
+</script>
+
+<style scoped>
+.background-color-fire {
+  background: linear-gradient(180deg, #fd7d24 50%, #fd7d24 50%);
+  background-color: #fd7d24;
+  color: white;
+}
+
+.card {
+  align-items: center;
+  border-radius: 4px;
+  display: inline-flex;
+}
+
+.card-left {
+  display: flex;
+}
+
+.card-right {
+  display: flex;
+  flex-direction: column;
+  padding-right: 32px;
+}
+
+.name {
+  font-size: 16px;
+  font-weight: 600;
+  text-transform: capitalize;
+}
+
+.number {
+  font-size: 14px;
+}
+</style>
--- projects/pokedex/pages/index.vue
+++ projects/pokedex/pages/index.vue
@@ -1,11 +1,39 @@
 <template>
   <div class="container">
-    <div></div>
+    <ul>
+      <li>
+        <Card :id="pokemon.id" :main-type="pokemon.types[0].type.name">
+          <template #image>
+            <img alt="" :src="pokemon.sprites.front_default" />
+          </template>
+
+          <template #name>{{ pokemon.name }}</template>
+        </Card>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script lang="ts">
+import axios from 'axios'
+import Card from '@/components/atoms/Card/Card.vue'
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  name: 'App',
+
+  components: { Card },
+
+  async asyncData() {
+    let pokemon: Array<any> = []
+
+    await axios.get('https://pokeapi.co/api/v2/pokemon/4/').then((response) => {
+      pokemon = response.data
+    })
+
+    return {
+      pokemon
+    }
+  }
+})
 </script>
