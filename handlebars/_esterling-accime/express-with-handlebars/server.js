1a0ddd3a199de2b72c9010e13b3711a7b8a20176
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-12-08 22:57:36 -0300

    chore: get started
    base source: https://www.youtube.com/watch?v=erfN7fH7A6s

---
+++ handlebars/_esterling-accime/express-with-handlebars/server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const exphbs = require('express-handlebars')
+
+app.engine(
+  '.hbs',
+  exphbs({
+    defaultLayout: null,
+    extname: '.hbs'
+  })
+)
+app.set('view engine', '.hbs')
+
+// Routing
+app.get('/', (req, res) => {
+  res.render('index')
+})
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+const appPort = 8080
+app.listen(appPort, () => {
+  console.log(`Server is starting at port ${appPort}`)
+})
