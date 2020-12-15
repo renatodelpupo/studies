25f26677f3e022f9f5058bf23580a36fd7dfd314
Renato Delpupo <16341269+renatodelpupo@users.noreply.github.com>
2020-12-15 16:03:24 -0300

    chore: green tests

--- test/_udemy/react-testing-with-jest-and-enzyme/jotto/src/Congrats.js
+++ test/_udemy/react-testing-with-jest-and-enzyme/jotto/src/Congrats.js
@@ -3,9 +3,23 @@ import React from 'react'
 /**
  * Functional React component for congratulatory message
  * @function
+ * @param {object} props - React props
  * @returns {JSX.Element} - Rendered component Rendered component (or null if `success` prop is false).
  */
 
-export default () => {
-  return <div></div>
+export default (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    )
+  } else {
+    return (
+      <div data-test="component-congrats">
+      </div>
+    )
+  }
 }
