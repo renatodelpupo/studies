41c61649d5ecc31c5307acd9df73c3490a32199c
Renato Delpupo <renatodelpupo@gmail.com>
2021-01-06 14:32:10 -0300

    chore: add guessed words prop types and setup

--- test/_udemy/react-testing-with-jest-and-enzyme/jotto/src/GuessedWords.test.js
+++ test/_udemy/react-testing-with-jest-and-enzyme/jotto/src/GuessedWords.test.js
@@ -1,4 +1,28 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, ShallowWrapper } from 'enzyme'
 import { checkProps, findByTestAttr } from '../test/testUtils'
 import GuessedWords from './GuessedWords'
+
+const defaultProps = {
+  guessedWords: [
+    {
+      guessedWord: 'train',
+      letterMatchCount: 3
+    }
+  ]
+}
+
+/**
+ * Factory function to create a ShallowWrapper for the GuessedWords component
+ * @function setup
+ * @param {object} props - Component props specific to this setup
+ * @returns {ShallowWrapper}
+ */
+const setup = (props = {}) => {
+  const setupProps = { ...defaultProps, ...props }
+  return shallow(<GuessedWords {...setupProps} />)
+}
+
+test('does not throw warning with expected props', () => {
+  checkProps(GuessedWords, defaultProps)
+})
