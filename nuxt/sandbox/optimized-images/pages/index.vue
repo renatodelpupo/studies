86d3a29cdb5f3e72315710e564917df6c33edc7d
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-02-25 20:20:07 -0300

    refactor: clean the layout

--- nuxt/sandbox/optimized-images/pages/index.vue
+++ nuxt/sandbox/optimized-images/pages/index.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">optimized-images</h1>
-
       <!-- Default  -->
       <figure class="picture">
         <picture>
@@ -52,25 +49,6 @@
         />
         <img alt="Open" src="~/assets/open--mobile.jpg" />
       </picture>
-
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
@@ -93,34 +71,6 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-@media (max-width: 576px) {
-  .title {
-    font-size: 50px;
-  }
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 
 .picture,
