d9dc706f361594bce45779fda5a1bb6c76fe735a
Renato Delpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-02-25 16:56:42 -0300

    chore: remove client conditional
    Run SSR with yarn generate need the asset URL transform on the server too; so the isClient check needs to be removed

--- nuxt/sandbox/optimized-images/nuxt.config.js
+++ nuxt/sandbox/optimized-images/nuxt.config.js
@@ -56,11 +56,9 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    extend(config, { isDev, isClient, loaders: { vue } }) {
-      if (isClient) {
-        vue.transformAssetUrls.img = ['data-src', 'src']
-        vue.transformAssetUrls.source = ['data-srcset', 'srcset']
-      }
+    extend(config, { isClient, isDev, loaders: { vue } }) {
+      vue.transformAssetUrls.img = ['data-src', 'src']
+      vue.transformAssetUrls.source = ['data-srcset', 'srcset']
     },
   },
 }
