226932cf213f2582bbeaacfc4f2a9217bfae4448
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-03-01 13:35:22 -0300

    chore: component test

---
+++ nuxt/sandbox/optimized-images/components/ResponsiveImage.vue
@@ -0,0 +1,36 @@
+<template>
+  <figure class="picture">
+    <picture>
+      <source :data-srcset="differentSizesWebP.srcSet" type="image/webp" />
+      <source :data-srcset="differentSizesOriginal.srcSet" type="image/png" />
+      <img :alt="alt" class="lazyload" :data-src="differentSizesOriginal.src" />
+    </picture>
+  </figure>
+</template>
+
+<script>
+export default {
+  name: 'ResponsiveImage',
+
+  props: {
+    alt: {
+      default: '',
+      type: String,
+    },
+    src: {
+      required: true,
+      type: String,
+    },
+  },
+
+  computed: {
+    differentSizesOriginal() {
+      return require(`${this.src}?resize&sizes[]=300&sizes[]=600&sizes[]=1000`)
+    },
+
+    differentSizesWebP() {
+      return require(`${this.src}?resize&sizes[]=300&sizes[]=600&sizes[]=1000&format=webp`)
+    },
+  },
+}
+</script>
---
+++ nuxt/sandbox/optimized-images/pages/component-preview.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ResponsiveImage alt="Dinosaur" src="~/assets/dinosaur.png" />
+
+    <div class="divider" />
+
+    <ResponsiveImage alt="Dinosaur 2" src="~/assets/dinosaur2.png" />
+
+    <div class="divider" />
+
+    <ResponsiveImage alt="Dinosaur 3" src="~/assets/dinosaur3.png" />
+
+    <div class="divider" />
+
+    <ResponsiveImage alt="Dinosaur 4" src="~/assets/dinosaur4.png" />
+
+    <div class="divider" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentPreview',
+}
+</script>
+
+<style scoped>
+.divider {
+  margin-top: 50vh;
+}
+</style>
