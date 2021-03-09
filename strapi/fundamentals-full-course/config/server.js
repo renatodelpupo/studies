df3d85bb8a4df7fd8fd42b1ad718b49a3fad7b67
Renato Delpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-03-09 14:41:51 -0300

    build: npx create-strapi-app --quickstart

---
+++ strapi/fundamentals-full-course/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9f1d21db9b60169acc58b2070b23cb13'),
+    },
+  },
+});
---
+++ strapi/fundamentals-full-course/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd20288ed-968a-4153-8926-e02b4d341d93'
+};
