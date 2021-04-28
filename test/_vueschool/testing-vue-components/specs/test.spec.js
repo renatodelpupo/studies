d9f942b3d3a0a937a59573cd6341692b9fc5a0e8
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-04-28 09:21:13 -0300

    chore(lesson-3): set data

--- test/_vueschool/testing-vue-components/specs/test.spec.js
+++ test/_vueschool/testing-vue-components/specs/test.spec.js
@@ -1,4 +1,5 @@
 import { mount } from '@vue/test-utils'
+import List from '@/list.vue'
 import TestComponent from '@/test.vue'
 
 test('mount a vue component', () => {
@@ -6,6 +7,16 @@ test('mount a vue component', () => {
     propsData: {
       value: 'VueSchool',
     },
+  })
+
+  expect(wrapper.html()).toMatchSnapshot()
+})
+
+test('renders list of movies', () => {
+  const wrapper = mount(List)
+  const movies = wrapper.vm.$data.marvelMovies
+  wrapper.setData({
+    marvelMovies: [...movies, 'Endgame'],
   })
 
   expect(wrapper.html()).toMatchSnapshot()
