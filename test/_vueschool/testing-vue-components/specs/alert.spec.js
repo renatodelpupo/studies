35fce8a5e53bd71c7bba0478c2f868dc19aa4a4d
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-04-29 13:49:06 -0300

    chore(lesson-6): lifecycle methods

--- test/_vueschool/testing-vue-components/specs/alert.spec.js
+++ test/_vueschool/testing-vue-components/specs/alert.spec.js
@@ -1,2 +1,36 @@
 import AlertMessage from '@/alert-message'
 import { mount } from '@vue/test-utils'
+
+describe('lifecycle methods', () => {
+  test('mounted assigns interval', () => {
+    const wrapper = mount(AlertMessage)
+
+    expect(wrapper.vm.interval).not.toBe(undefined)
+  })
+
+  test('counter works', () => {
+    jest.useFakeTimers() // allow timer
+
+    const wrapper = mount(AlertMessage)
+
+    expect(wrapper.vm.counter).toBe(0)
+
+    jest.advanceTimersByTime(1000)
+    expect(wrapper.vm.counter).toBe(1)
+
+    jest.advanceTimersByTime(2000)
+    expect(wrapper.vm.counter).toBe(3)
+  })
+
+  test('instance gets destroyed', () => {
+    jest.useFakeTimers() // allow timer
+
+    const beforeDestroyedSpy = jest.spyOn(AlertMessage, 'beforeDestroy')
+    const wrapper = mount(AlertMessage)
+
+    wrapper.vm.counter = wrapper.vm.timer - 1
+    jest.advanceTimersByTime(1000)
+
+    expect(beforeDestroyedSpy).toHaveBeenCalled()
+  })
+})
