accff23a8a4252204a5e987ba8c10d3466ca2ffc
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-04-29 14:37:24 -0300

    chore(lesson-7): traversing the DOM

--- test/_vueschool/testing-vue-components/specs/fruits.spec.js
+++ test/_vueschool/testing-vue-components/specs/fruits.spec.js
@@ -1,2 +1,24 @@
 import  FruitBasket from '@/fruit-basket'
 import { mount } from '@vue/test-utils'
+
+test('can add fruits to basket with DOM', () => {
+  const wrapper = mount(FruitBasket)
+  const input = wrapper.find('input')
+  const button = wrapper.find('button')
+
+  // Starts with empty list
+  expect(wrapper.findAll('li').length).toBe(0)
+
+  // Fill new fruit
+  input.element.value = 'banana'
+  input.trigger('input') // trig v-model
+  expect(wrapper.vm.fruit).toBe('banana')
+
+  // Add new fruit
+  button.trigger('click')
+  expect(wrapper.vm.fruit).toBe('') // input was cleaned
+  expect(wrapper.vm.basket).toEqual(expect.arrayContaining(['banana']))
+
+  // Ends with a fruit
+  // expect(wrapper.findAll('li').length).toBe(1)
+})
