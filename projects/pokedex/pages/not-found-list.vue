904b98dcfe68ee47c22e0ff31046ead8812545af
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-06-13 18:44:43 -0300

    chore: create not found list page

---
+++ projects/pokedex/pages/not-found-list.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="container">
+    <ul>
+      <li v-for="pokemonSpecie in pokemonSpecieList" :key="pokemonSpecie[0].name">
+        <Card
+          v-for="pokemon in pokemonSpecie"
+          :id="pokemon.id"
+          :key="pokemon.name"
+          :main-type="pokemon.types[0].type.name"
+        >
+          <template #image>
+            <img alt="" :src="pokemon.sprites.front_default" />
+          </template>
+
+          <template #name>{{ pokemon.name }}</template>
+        </Card>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Card from '@/components/atoms/Card/Card.vue'
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'App',
+
+  components: { Card },
+
+  async asyncData() {
+    const pokemonsNotFoundIds = [2, 6]
+
+    const addPokemonAncestorName = (pokemonName) => {
+      return fetch(`https://pokeapi.co/api/v2/pokemon-species/${pokemonName}`)
+        .then((response) => response.json())
+        .then((data) => data.evolves_from_species.name)
+        .then((pokemonAncestorName) => [pokemonAncestorName, pokemonName])
+    }
+
+    const getPokemonInfo = (pokemon) => {
+      return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`).then((response) => response.json())
+    }
+
+    const getPokemonAndAncestorName = (pokemon) => {
+      return getPokemonInfo(pokemon)
+        .then((data) => data.name)
+        .then(addPokemonAncestorName)
+    }
+
+    const pokemonSpecieList = await Promise.all(pokemonsNotFoundIds.map(getPokemonAndAncestorName)).then(
+      (pokemonsList) => {
+        return Promise.all(
+          pokemonsList.map((item) => {
+            return Promise.all(item.map(getPokemonInfo))
+          })
+        )
+      }
+    )
+
+    return {
+      pokemonSpecieList
+    }
+  }
+})
+</script>
+
+<style scoped>
+li {
+  display: flex;
+  gap: 10px;
+  margin: 10px 0;
+}
+</style>
