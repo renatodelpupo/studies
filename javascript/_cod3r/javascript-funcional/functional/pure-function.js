79f82788c79243094854f54a30bffe3deb15446d
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-06-24 16:03:03 -0300

    chore(section-4): pure function

---
+++ javascript/_cod3r/javascript-funcional/functional/pure-function.js
@@ -0,0 +1,10 @@
+// Pure Function
+function sum(a, b) {
+  return a + b
+}
+
+console.log(sum(7, 14))
+
+// Pure Function
+// Functions that don't depend on or interfere with any other data
+// Always returns the same result if the same arguments are passed, without any observable side effects
