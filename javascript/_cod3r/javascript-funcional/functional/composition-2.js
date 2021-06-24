8fa8acad51e559aedffcbc742c74bd768fd6cb68
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-06-24 19:08:42 -0300

    chore(lesson-80): composition

---
+++ javascript/_cod3r/javascript-funcional/functional/composition-2.js
@@ -0,0 +1,28 @@
+function isPromise(fn) {
+  return Promise.resolve(fn) === fn
+}
+
+function composition(...fns) {
+  return function (value) {
+    return fns.reduce(async (acc, fn) => {
+      if (isPromise(acc)) {
+        return fn(await acc)
+      } else {
+        return fn(acc)
+      }
+    }, value)
+  }
+}
+
+const upperCase = (text) => text.toUpperCase()
+const emphasis = (text) => `${text}!!`
+const slow = (text) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(text.split('').join(' '))
+    }, 3000)
+  })
+}
+
+composition(upperCase, emphasis)('hey').then(console.log)
+composition(upperCase, emphasis, slow)('hey').then(console.log)
