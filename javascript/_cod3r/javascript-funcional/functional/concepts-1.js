a68297a1b9a1bbd7b7eaf7ea9f06decb18e6e51d
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-06-24 15:39:37 -0300

    chore(section-4): concepts of closure, currying and higher-order function

---
+++ javascript/_cod3r/javascript-funcional/functional/concepts-1.js
@@ -0,0 +1,40 @@
+function isBetween(min, max) {
+  // Currying && Higher-Order Function
+  return function handleError(error) {
+    // Currying && Higher-Order Function
+    return function validateNumber(number) {
+      if (number < min || number > max) {
+        // Closure
+        throw error
+      }
+    }
+  }
+}
+
+// Currying && Higher-Order Function
+function validation(fn) {
+  // Currying && Higher-Order Function
+  return function (value) {
+    try {
+      // Closure
+      fn(value)
+    } catch (e) {
+      return { error: e }
+    }
+  }
+}
+
+const is20Century = isBetween(1901, 2000)
+const handle20CenturyError = is20Century('The year is invalid')
+const test20Century = validation(handle20CenturyError)
+
+console.log(test20Century(2001))
+
+// Closure
+// Function that remember lexical scope even if is executed outside
+
+// Currying
+// Chain of functions that accepts just a single argument
+
+// Higher-Order Function
+// Functions with functions as arguments or return
