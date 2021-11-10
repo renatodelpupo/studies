96e61c950552b4dfa73823f5bbfec0ac1a37c90b
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2021-11-10 09:44:34 -0300

    build: setup new project from boilerplate

---
+++ next/_willian-justen/next-graphql-leaflet/my-trips/src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Title>Welcome to Next.js!</S.Title>
+    <S.Description>Just another website</S.Description>
+  </S.Wrapper>
+)
+
+export default Main
