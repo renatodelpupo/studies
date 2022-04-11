bd40b1040125361503b3f4764f65166c8e2a5c32
Renato Delpupo <16341269+renatodelpupo@users.noreply.github.com>
2022-04-11 11:38:49 -0300

    chore: try getting props

--- next/_willian-justen/next-graphql-leaflet/my-trips/src/pages/about.tsx
+++ next/_willian-justen/next-graphql-leaflet/my-trips/src/pages/about.tsx
@@ -1,5 +1,22 @@
+import client from 'graphql/client'
+import { GET_PAGES } from 'graphql/queries'
 import AboutTemplate from 'templates/About'
 
-const About = () => <AboutTemplate />
+export default function AboutPage() {
+  return <AboutTemplate />
+}
 
-export default About
+export const getStaticProps = async () => {
+  const { pages } = await client.request(GET_PAGES)
+
+  console.log(pages)
+
+  return {
+    props: {},
+  }
+}
+
+// getStaticPaths => generate URLs on build time
+// getStaticProps => get page data (props) on build time / static
+// getServerSideProps => get page data (props) on run time / every request / bundle only on server
+// getInitialProps => get page data (props) on run time / every request / bundle available on client / hydrate
