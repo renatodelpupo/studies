5b4aad514d98f3cc799192e0f9cc24e44669b3d4
Renato Delpupo <16341269+renatodelpupo@users.noreply.github.com>
2022-04-11 11:42:36 -0300

    style: format document

--- next/_willian-justen/next-graphql-leaflet/my-trips/src/pages/_app.tsx
+++ next/_willian-justen/next-graphql-leaflet/my-trips/src/pages/_app.tsx
@@ -14,7 +14,7 @@ function App({ Component, pageProps }: AppProps) {
         <meta name="theme-color" content="#06092b" />
 
         <link
-          crossorigin=""
+          crossOrigin=""
           href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
           integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
           rel="stylesheet"
