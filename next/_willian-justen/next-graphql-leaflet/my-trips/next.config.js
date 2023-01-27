73745cd6c42aaaca008ce6f5d86ecba513041671
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2023-01-26 21:38:29 -0300

    chore: add close button and format images

--- next/_willian-justen/next-graphql-leaflet/my-trips/next.config.js
+++ next/_willian-justen/next-graphql-leaflet/my-trips/next.config.js
@@ -2,6 +2,9 @@ const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  images: {
+    domains: ['media.graphcms.com'],
+  },
   pwa: {
     dest: 'public',
     disable: !isProd,
