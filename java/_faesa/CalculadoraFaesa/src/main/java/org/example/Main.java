c381f56a68a9f08a06b45d59e179a3708c023389
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2024-11-10 15:51:24 -0300

    feat: bind screen form elements

--- java/_faesa/CalculadoraFaesa/src/main/java/org/example/Main.java
+++ java/_faesa/CalculadoraFaesa/src/main/java/org/example/Main.java
@@ -1,6 +1,15 @@
 package org.example;
 
+import javax.swing.*;
+
+import org.example.view.Screen;
+
 public class Main {
   public static void main(String[] args) {
+    JFrame frame = new JFrame("Screen");
+    frame.setContentPane(new Screen().calculator);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
   }
 }
--- java/_faesa/CalculadoraFaesa/src/main/java/org/example/view/Screen.java
+++ java/_faesa/CalculadoraFaesa/src/main/java/org/example/view/Screen.java
@@ -2,4 +2,24 @@ package org.example.view;
 
 import javax.swing.*;
 
-public class Screen extends JFrame {}
+public class Screen extends JFrame {
+  private JButton actionClean;
+  private JButton number0;
+  private JButton number1;
+  private JButton number2;
+  private JButton number3;
+  private JButton number4;
+  private JButton number5;
+  private JButton number6;
+  private JButton number7;
+  private JButton number8;
+  private JButton number9;
+  private JButton operatorAddition;
+  private JButton operatorDivision;
+  private JButton operatorEquals;
+  private JButton operatorMultiplication;
+  private JButton operatorSubtraction;
+  public JPanel calculator;
+  private JPanel keyboard;
+  private JTextField visor;
+}
