acef9e2c46c3b9d1a10e6e260bfd608702f51b41
renatodelpupo <16341269+renatodelpupo@users.noreply.github.com>
2024-11-10 16:05:44 -0300

    feat: print numbers on the display

--- java/_faesa/CalculadoraFaesa/src/main/java/org/example/view/Screen.java
+++ java/_faesa/CalculadoraFaesa/src/main/java/org/example/view/Screen.java
@@ -1,6 +1,8 @@
 package org.example.view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Screen extends JFrame {
   private JButton actionClean;
@@ -21,5 +23,95 @@ public class Screen extends JFrame {
   private JButton operatorSubtraction;
   public JPanel calculator;
   private JPanel keyboard;
-  private JTextField visor;
+  private JTextField display;
+
+  private void printOnDisplay(String newInput) {
+    String visorValue = display.getText();
+
+    if (visorValue.equals("0")) {
+      display.setText(newInput);
+      return;
+    }
+
+    display.setText(visorValue + newInput);
+  }
+
+  public Screen() {
+    actionClean.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        display.setText("0");
+      }
+    });
+
+    number0.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number0.getText());
+      }
+    });
+
+    number1.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number1.getText());
+      }
+    });
+
+    number2.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number2.getText());
+      }
+    });
+
+    number3.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number3.getText());
+      }
+    });
+
+    number4.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number4.getText());
+      }
+    });
+
+    number5.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number5.getText());
+      }
+    });
+
+    number6.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number6.getText());
+      }
+    });
+
+    number7.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number7.getText());
+      }
+    });
+
+    number8.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number8.getText());
+      }
+    });
+
+    number9.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        printOnDisplay(number9.getText());
+      }
+    });
+  }
 }
